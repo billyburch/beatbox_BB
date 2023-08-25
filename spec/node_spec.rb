af7e447cf24dc6aa1a6cd5f9ff861a703485d763
Billy Burch <billyburch40@gmail.com>
2023-08-24 21:11:08 -0600

    add: implementation code to pass argument

--- spec/node_spec.rb
+++ spec/node_spec.rb
@@ -1,5 +1,5 @@
-require "./lib/node"
-require "pry"
+require './lib/node'
+require 'pry'
 require 'rspec'
 
 RSpec.describe 'Node' do
@@ -7,4 +7,15 @@ RSpec.describe 'Node' do
     node = Node.new("plop")
     expect(node).to be_a(Node)
   end
+
+  it 'returns instance argument' do
+    node = Node.new("plop")
+    expect(node.data).to eq("plop")
+  end
+
+  xit 'returns nil for next node' do
+    node = Node.new("plop")
+    expect(node.next_node).to eq(nil)
+  end
+
 end
